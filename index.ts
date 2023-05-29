a603679d73dd3d8881586fc8e9b196a2f7898d9b
jajima <jajima.jp@gmail.com>
2023-05-29 17:07:33 +0900

    Update index
    print1を実行するインタプリタの実装

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { print1 } from './testcase';
 import { getCodel } from './getCodel';
+import { getColorBlockValue } from './getColorBlockValue';
 
 const hues = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta', 'black', 'white'] as const;
 export type Hue = typeof hues[number];
@@ -7,6 +8,42 @@ const brightnesses = ['light', 'normal', 'dark'] as const;
 export type Blightness = typeof brightnesses[number];
 export type Direction = 'right' | 'down' | 'left' | 'up';
 export type LR = 'left' | 'right';
+
+// clockwise
+const nextDP = (dp: Direction) => {
+  switch (dp) {
+    case 'right':
+      return 'down';
+    case 'down':
+      return 'left';
+    case 'left':
+      return 'up';
+    case 'up':
+      return 'right';
+  }
+}
+
+// anticlockwise
+const previousDP = (dp: Direction) => {
+  switch (dp) {
+    case 'right':
+      return 'up';
+    case 'down':
+      return 'right';
+    case 'left':
+      return 'down';
+    case 'up':
+      return 'left';
+  }
+}
+
+// rotates the DP clockwise that many steps (anticlockwise if negative).
+// TODO: 整数に変換して剰余を取れば高速化できる
+const rotateDP = (curr: Direction, amount: number): Direction => {
+  if (amount === 0) { return curr }
+  else if (amount < 0) { return rotateDP(previousDP(curr), amount + 1) }
+  else /* amount > 0 */ { return rotateDP(nextDP(curr), amount - 1) }
+}
 
 export interface Position {
   x: number;
@@ -48,6 +85,8 @@ class PietInterpreter {
   private dp: Direction;
   private cc: LR;
   private stack: number[];
+  private previousCodelSize: number;
+  private output: string;
 
   constructor(private image: PietImage) {
     this.x = 0;
@@ -55,6 +94,8 @@ class PietInterpreter {
     this.dp = 'right';
     this.cc = 'left';
     this.stack = [];
+    this.previousCodelSize = 0;
+    this.output = '';
   }
 
   private static colorDifference(from: Color, to: Color): [number, number] {
@@ -70,15 +111,28 @@ class PietInterpreter {
   }
 
   private move(): void {
+    let newX = this.x,  newY = this.y;
     if (this.dp === 'right') {
-      this.x++;
+      newX++;
     } else if (this.dp === 'down') {
-      this.y++;
+      newY++;
     } else if (this.dp === 'left') {
-      this.x--;
+      newX--;
     } else if (this.dp === 'up') {
-      this.y--;
+      newY--;
     }
+    if (this.image.getColor(newX, newY) === undefined) {
+      throw Error('edge is curreltly not supported');
+    }
+    if (this.image.getColor(newX, newY)?.hue === 'white') {
+      throw Error('white is curreltly not supported');
+    }
+    if (this.image.getColor(newX, newY)?.hue === 'black') {
+      throw Error('black is curreltly not supported');
+    }
+    const newPos = getCodel(this.image, newX, newY, this.dp, this.cc);
+    this.x = newPos.x;
+    this.y = newPos.y;
   }
 
   private executeCommand(from: Color, to: Color): void {
@@ -90,18 +144,38 @@ class PietInterpreter {
         const op2 = this.stack[this.stack.length - 2];
         this.stack.splice(this.stack.length, 1, op1 + op2);
       }
+    } else if (hueDiff === 0 && lightnessDiff === 1) { // push
+      this.stack.push(this.previousCodelSize);
+    } else if (hueDiff === 5 && lightnessDiff === 1) { // out(number)
+      const top = this.stack.pop();
+      if (top !== undefined) {
+        this.output += top.toString();
+      }
     }
     // TODO 他のコマンドも実装する
   }
 
   public run(): void {
-    while (this.x >= 0 && this.y >= 0 && this.x < this.image.width && this.y < this.image.height) {
-      const currentColor = this.image.getColor(this.x, this.y);
-      this.move();
-      const nextColor = this.image.getColor(this.x, this.y);
-      if (currentColor !== undefined && nextColor !== undefined) {
-        this.executeCommand(currentColor, nextColor);
+    let steps = 0;
+    try {
+      while (this.x >= 0 && this.y >= 0 && this.x < this.image.width && this.y < this.image.height) {
+        const currentColor = this.image.getColor(this.x, this.y);
+        this.previousCodelSize = getColorBlockValue(this.image, this.x, this.y);
+        this.move();
+        const nextColor = this.image.getColor(this.x, this.y);
+        if (currentColor !== undefined && nextColor !== undefined) {
+          this.executeCommand(currentColor, nextColor);
+        }
+        steps++;
+        if (steps > 100) {
+          console.log('steps > 100');
+          return;
+        }
       }
+    } catch(e) {
+    } finally {
+      console.log(this.output);
+      console.log({ stack: this.stack });
     }
   }
 }
@@ -127,4 +201,8 @@ const ppProgram = (program: PietImage) => {
 };
 ppProgram(print1);
 
-console.log(getCodel(print1, 1, 0, 'right', 'left'));
+console.log(getCodel(print1, 1, 0, 'right', 'left')); // should be (2, 2)
+console.log(getColorBlockValue(print1, 1, 1)) // should be 4
+
+const it = new PietInterpreter(print1);
+it.run();
